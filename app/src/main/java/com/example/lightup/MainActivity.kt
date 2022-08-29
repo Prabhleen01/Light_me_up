8547ab30e9b345b3ac77b2e54e1e0396564ec1a7
Prabhleen01 <prabhleenk2001@gmail.com>
2022-08-29 22:26:36 +1000

    SaveInstanceDone

--- app/src/main/java/com/example/lightup/MainActivity.kt
+++ app/src/main/java/com/example/lightup/MainActivity.kt
@@ -2,8 +2,9 @@ package com.example.lightup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.ImageView
-
+const val KeyImage = "Image"
 class MainActivity : AppCompatActivity() {
     var state = R.drawable.ic_lock_black_24dp
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val imageView = findViewById<ImageView>(R.id.locked)
+        savedInstanceState?.let{
+            state= it.getInt(KeyImage)
+            imageView.setImageDrawable(getDrawable(state))
 
+        }
         imageView.setOnLongClickListener {
             state = when (state) {
                 R.drawable.ic_lock_black_24dp -> R.drawable.ic_favorite_border_black_24dp
@@ -22,5 +27,10 @@ class MainActivity : AppCompatActivity() {
             imageView.setImageDrawable(getDrawable(state))
             true
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KeyImage, state)
     }
 }
